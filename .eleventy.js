408572f406ced7ddfaf2090d807f0cfde197cf63
thebrodaily <lcolonnjtx@gmail.com>
2025-08-16 13:43:25 -0500

    Update eleventy

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,12 @@
 module.exports = function(eleventyConfig) {
-  // This tells Eleventy to look for files in the root directory
-  // and process them.
+  
+  // This line tells Eleventy to copy the 'admin' folder and its contents 
+  // to the final build. This is crucial for your CMS to work.
+  eleventyConfig.addPassthroughCopy("admin");
+
+  // You can add other folders here if you create them later, like 'assets' for images.
+  // eleventyConfig.addPassthroughCopy("assets");
+
   return {
     dir: {
       input: ".",
